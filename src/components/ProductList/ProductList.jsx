ef4e71f23fe43f7e5bbe0380ee8d995bda50d76f
maxkolt <12345kolt@gmail.com>
2025-01-14 23:34:05 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,10 +6,10 @@ import {useCallback, useEffect} from "react";
 
 
 const products = [
-    {id: '1', title: 'Сумка Fila', price: 1000, description: 'Оригинал', img: '/img/IMAGE 1.jpg'},
-    {id: '2', title: 'Сумка Fila', price: 700, description: 'Оригинал, новая', img: '/img/IMAGE 2.jpg'},
-    {id: '3', title: 'Сумка Puma', price: 1000, description: 'Черного цвета, оригинал', img: '/img/IMAGE 3.jpg'},
-    {id: '4', title: 'Телефон samsung A15', price: 15000, description: 'Новый, на гарантииНовый, на гарантииНовый, на гарантииНовый, на гарантииНовый, на гарантииНовый, на гарантииНовый, на гарантииНовый, на гарантииНовый, на гарантии', img: '/img/IMAGE 4.jpg'},
+    {id: '1', title: 'Сумка Fila', price: 1000, description: 'ОригиналЧерного цвета, оригиналЧерного цвета', img: '/img/IMAGE 1.jpg'},
+    {id: '2', title: 'Сумка Fila', price: 700, description: 'Оригинал, новая Черного цвета, оригиналЧерного цвета, оригинал', img: '/img/IMAGE 2.jpg'},
+    {id: '3', title: 'Сумка Puma', price: 1000, description: ' Черного цвета, оригинал Черного цвета, оригинал', img: '/img/IMAGE 3.jpg'},
+    {id: '4', title: 'Телефон samsung A15', price: 15000, description: 'Новый, на гарантииНовый, на гарантииНовый, на гарантииНовыйЧерного цвета, оригиналЧерного цвета, оригинал', img: '/img/IMAGE 4.jpg'},
 ]
 
 
