b2b4a407b7296226f0b8a083a35957beb29eed71
maxkolt <12345kolt@gmail.com>
2025-01-14 15:29:08 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,12 +6,78 @@ import {useCallback, useEffect} from "react";
 
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые', img: '/img/IMAGE 1.jpg'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая', img: '/img/IMAGE 2.jpg'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые', img: '/img/IMAGE 3.jpg'},
+    {id: '1', title: 'Сумка Fila', price: 1000, description: 'Оригинал', img: '/img/IMAGE 1.jpg'},
+    {id: '2', title: 'Сумка Fila', price: 700, description: 'Оригинал, новая', img: '/img/IMAGE 2.jpg'},
+    {id: '3', title: 'Сумка Puma', price: 1000, description: 'Черного цвета, оригинал', img: '/img/IMAGE 3.jpg'},
+    {id: '4', title: 'Телефон samsung A15', price: 15000, description: 'Новый, на гарантии', img: '/img/IMAGE 4.jpg'},
 ]
 
 
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
 
+const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('http://85.119.146.179:8000/web-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [addedItems])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
+    return (
+        <div className={'list'}>
+            {products.map(item => (
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
+        </div>
+    );
+};
 
 export default ProductList;
