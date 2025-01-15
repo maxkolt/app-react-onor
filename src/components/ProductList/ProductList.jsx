e444e0b87dde4598adf8f000148563b5522e2cfb
maxkolt <12345kolt@gmail.com>
2025-01-15 22:09:39 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,10 +3,10 @@ import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 
 const products = [
-    { id: '1', title: 'Сумка Fila', price: 1000, description: 'ОригиналЧерного цвета, оригиналЧерного цвета', img: '/img/IMAGE 1.jpg' },
+    { id: '1', title: 'Сумка Fila', price: 1000, description: 'ОригиналЧерного цвета, оригиналЧерного цвета Новый, на гарантии. НовыйЧерного цвета, оригинал', img: '/img/IMAGE 1.jpg' },
     { id: '2', title: 'Сумка Fila', price: 700, description: 'Оригинал, новая Черного цвета, оригиналЧерного цвета, оригинал', img: '/img/IMAGE 2.jpg' },
     { id: '3', title: 'Сумка Puma', price: 1000, description: 'Черного цвета, оригинал Черного цвета, оригинал', img: '/img/IMAGE 3.jpg' },
-    { id: '4', title: 'Телефон Samsung A15Samsung A15Samsung A15', price: 15000, description: 'Новый, на гарантии. НовыйЧерного цвета, оригинал.', img: '/img/IMAGE 4.jpg' },
+    { id: '4', title: 'Samsung A15', price: 15000, description: 'Новый, на гарантии. НовыйЧерного цвета, оригинал овыйЧерного цвета, оригиналовыйЧерного цвета, оригинал.', img: '/img/IMAGE 4.jpg' },
 ];
 
 const App = () => {
